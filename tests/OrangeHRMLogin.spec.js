fdf3619afa27a4744cfb4663b1692af7ce2fd422
PayalS <pvsshreegajanan@gmail.com>
2026-05-26 17:03:02 +0530

    First code push

---
+++ tests/OrangeHRMLogin.spec.js
@@ -0,0 +1,19 @@
+const{test,expect} = require('@playwright/test')
+
+test("OrangeHRMLogin" , async function ({page}) 
+{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.getByPlaceholder("Username").type("Admin",{delay:200}  )
+    await page.getByPlaceholder("Password").type("admin123", {delay:100})
+    await page.locator("//button[@type='submit']").click()
+
+    //await page.waitForTimeout(5000)
+
+    await expect(page).toHaveURL(/dashboard/)
+
+    await page.getByAltText("profile picture").first().click()
+    await page.getByText("Logout").click()
+    await page.waitForTimeout(3000)
+
+    await expect(page).toHaveURL(/login/)
+})
---
+++ tests/sample.spec.js
@@ -0,0 +1,20 @@
+const{test,expect} = require('@playwright/test')
+
+//First test case
+
+test("First test case",async function({page})
+{
+
+})
+
+
+//Second test
+test("second test", async function ({page}) {
+    
+})
+
+
+//third test
+test("Third test", async function ({page}) {
+    
+})
