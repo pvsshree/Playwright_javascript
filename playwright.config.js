01917ea545a0cc2e3a70eb841ca87850abc34b01
PayalS <pvsshreegajanan@gmail.com>
2026-05-27 18:18:48 +0530

    Trace/Screenshot/video

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,12 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+        //viewport:{width:1920, height:1080},
+    screenshot:"on",
+  video:"on",
+trace:"on" },
+      
     },
 
    /* {
--- tests/OrangeHRMLogin.spec.js
+++ tests/OrangeHRMLogin.spec.js
@@ -1,8 +1,11 @@
 const{test,expect} = require('@playwright/test')
 
+test.use({viewportSize:{width:1200,height:800}})
 test("OrangeHRMLogin" , async function ({page}) 
 {
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    console.log(await page.viewportSize().width)
+    console.log(await page.viewportSize().height)
     await page.getByPlaceholder("Username").type("Admin",{delay:200}  )
     await page.getByPlaceholder("Password").type("admin123", {delay:100})
     await page.locator("//button[@type='submit']").click()
@@ -13,7 +16,7 @@ test("OrangeHRMLogin" , async function ({page})
 
     await page.getByAltText("profile picture").first().click()
     await page.getByText("Logout").click()
-    await page.waitForTimeout(3000)
+   // await page.waitForTimeout(3000)
 
     await expect(page).toHaveURL(/login/)
 })
