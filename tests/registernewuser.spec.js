8357a15ace2107bd3889ed5f5bcb739053dec8b2
PayalS <pvsshreegajanan@gmail.com>
2026-05-28 23:41:20 +0530

    new user updated

--- tests/registernewuser.spec.js
+++ tests/registernewuser.spec.js
@@ -8,10 +8,30 @@ await page.locator("#state").selectOption({label:"Goa"})
 await page.waitForTimeout(3000)
 await page.locator("#state").selectOption({value:"Assam"})
 await page.waitForTimeout(3000)
-const value = await page.locator("#state").textContent()
-console.log(value)
+//const value = await page.locator("#state").textContent()
+//console.log(value)
 
-await expect(value.includes("Manipur")).toBeTruthy
+//await expect(value.includes("Manipur")).toBeTruthy
+
+
+const state = await page.$('#state')
+const allElements = await state.$$('option')
+//For Assertion to check state is present or not
+const ddStatus = false
+
+//To get all Elements
+for(let i=0 ; i<allElements.length ; i++)
+{
+const element = await allElements[i]
+const value = await element.textContent()
+console.log(value);
+
+}
+
+
+
+await page.locator('#hobbies').selectOption(['Playing','Swimming'])
+await page.waitForTimeout(3000)
 
 
 })
